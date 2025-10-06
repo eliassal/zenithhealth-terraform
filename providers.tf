81447d1e8d0133d557bb4c9312ff68a03d0a1943
eliassal <salamadmin@free.fr>
2025-10-06 16:53:03 +0200

    4th commit

--- providers.tf
+++ providers.tf
@@ -15,10 +15,10 @@ provider "azurerm" {
     }
   }
 
-  subscription_id = "f151ee3f-4725-460d-8e3b-82512dfda843"
-  tenant_id       = "6799c70e-3ceb-4e88-af13-8f6c565fd4a5"
-  client_id       = "90323805-8d22-46d3-842c-3b991744d1bb"
-  client_secret   = "bXJ8Q~P.4qOgedWO_9ARjvc7gz3Pe6CmohgRlcYo"
+  subscription_id = ""
+  tenant_id       = ""
+  client_id       = ""
+  client_secret   = ""
 
   resource_provider_registrations = "none"
 }
